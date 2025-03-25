19ed279f50c8247372bd12ba262b4344fb26b422
Victor <victorlzbu@gmail.com>
2025-03-25 20:06:42 -0300

    chore: simplify kube credentials

--- provider.tf
+++ provider.tf
@@ -26,23 +26,11 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-    command     = "aws"
-  }
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
-      command     = "aws"
-    }
+    config_path = "~/.kube/config"
   }
 }
